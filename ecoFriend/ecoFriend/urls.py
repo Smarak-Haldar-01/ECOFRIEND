28b1c27986f09ab144a2d97d4658b7ac796b4508
Rudra356 <rudranaskar66@gmail.com>
2025-03-22 07:25:31 +0530

    Updated dashboard with responsive Bootstrap design

--- ecoFriend/ecoFriend/urls.py
+++ ecoFriend/ecoFriend/urls.py
@@ -7,5 +7,7 @@ from ecoFriend import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('login-signin/', views.login_signin_home, name="login_signin")
+    path('login-signin/', views.login_signin_home, name="login_signin"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecoFriend/ecoFriend/views.py
+++ ecoFriend/ecoFriend/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-  return render(request, 'home/home.html', {'title':'HOME | ECO-Friend'})
+    return render(request, 'home/home.html', {'title': 'HOME | ECO-Friend'})
 
 def login_signin_home(request):
-  return render(request, 'home/login_signin.html', {'title':'Login-Signin | ECO-Friend'})
+    return render(request, 'home/login_signin.html', {'title': 'Login-Signin | ECO-Friend'})
+
+def dashboard(request):  
+    return render(request, 'home/dashboard.html', {'title': 'Dashboard | ECO-Friend'})  # Ensure the template exists
